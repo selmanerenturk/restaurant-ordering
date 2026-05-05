17632270874ce175fb8ef11457b9782b2fecc937
selman <selmanerenturk@gmail.com>
2026-05-05 12:07:22 +0300

    review

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,9 +8,7 @@ import './index.css';
 import App from './App';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </StrictMode>
 );
